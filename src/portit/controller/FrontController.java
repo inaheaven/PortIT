1ced2896074df40bdebdc3a772810171438fea80
gnsngck <gyngskna@gmail.com>
2017-06-12 00:11:05 +0900

    패키지 이름 설정, package-info.java 설명 추가

---
+++ src/portit/controller/FrontController.java
@@ -0,0 +1,21 @@
+package portit.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/*")
+public class FrontController extends HttpServlet {
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("Servlet Loaded");
+		resp.setContentType("text/html; charset=UTF-8");		
+		
+	}
+
+}
