a9ee823dab87fae77c7d708c950d8fe7e646984a
gnsngck <gnsngck@gmail.com>
2017-06-12 19:12:48 +0900

    태그 DTO 작성

--- src/portit/model/dto/Tag.java
+++ src/portit/model/dto/Tag.java
@@ -1,5 +1,36 @@
 package portit.model.dto;
 
+/**
+ * 태그 값 객체
+ * @author gnsngck
+ *
+ */
 public class Tag {
 
+	private int tag_id;
+	private String tag_type;
+	private String tag_name;
+	public int getTag_id() {
+		return tag_id;
+	}
+	public String getTag_type() {
+		return tag_type;
+	}
+	public String getTag_name() {
+		return tag_name;
+	}
+	
+	public Tag setTag_id(int tag_id) {
+		this.tag_id = tag_id;
+		return this;
+	}
+	public Tag setTag_type(String tag_type) {
+		this.tag_type = tag_type;
+		return this;
+	}
+	public Tag setTag_name(String tag_name) {
+		this.tag_name = tag_name;
+		return this;
+	}
+	
 }
