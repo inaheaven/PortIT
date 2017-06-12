d486953a87d54a722cb80d1fdbc66cff56cdba65
Gyngskna <gyngskna@gmail.com>
2017-06-12 09:13:26 +0900

    문법오류 수정

--- src/portit/controller/FollowController.java
+++ src/portit/controller/FollowController.java
@@ -1,20 +1,21 @@
 package portit.controller;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class FollowController extends HttpServlet {
 	
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException ServletException {
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
 		doPost(req, resp);
 	}
 
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException ServletException {
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
 		System.out.println("Servlet Loaded");
-		
-		req.setCharacterEncoding("utf-8");
-		resp.setCharacterEncoding("utf-8");
+		resp.setContentType("text/html; charset=UTF-8");
 	}
 
 }
--- src/portit/controller/FrontController.java
+++ src/portit/controller/FrontController.java
@@ -8,13 +8,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/*")
+@WebServlet("/")
 public class FrontController extends HttpServlet {
 	
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println("Servlet Loaded");
-		resp.setContentType("text/html; charset=UTF-8");		
+		resp.setContentType("text/html; charset=UTF-8");
 		
 	}
 
