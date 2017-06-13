39612879f1ba55ae67c4f4fcf5a863602ef9df82
InSoo <dtr90@naver.com>
2017-06-13 14:07:36 +0900

    a

--- src/portit/controller/SearchController.java
+++ src/portit/controller/SearchController.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns="/join")
+//@WebServlet(urlPatterns="/join")
 public class SearchController extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
