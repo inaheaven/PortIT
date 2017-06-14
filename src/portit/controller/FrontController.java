fd06912b701a537ed891909d65269f1c32dbb954
gnsngck <gyngskna@gmail.com>
2017-06-15 00:24:12 +0900

    컨트롤러 인터페이스 제작, 팩토리 메서드 패턴에 따른 인스턴스 생성, 앞의 내용을 프론트 컨트롤러에 반영

--- src/portit/controller/FrontController.java
+++ src/portit/controller/FrontController.java
@@ -13,12 +13,12 @@ import portit.model.dto.Member;
 
 /**
  * 프론트 컨트롤러
- * 
+ * 클라이언트의 서블릿 요청을 받아 기능을 해당 컨트롤러에게 분배하고 해당 컨트롤러의 처리 결과를 뷰에게 전달
  * @author gnsngck
  *
  */
 @SuppressWarnings("serial")
-@WebServlet("/*.do") /* HTML/JSP에서 .do로 끝나는 요청은 모두 서블릿 요청으로 간주하여 프론트 컨트롤러가 처리 */
+@WebServlet("*.do") /* HTML/JSP에서 .do로 끝나는 요청은 모두 서블릿 요청으로 간주하여 프론트 컨트롤러가 처리 */
 public class FrontController extends HttpServlet {
 
 	@Override
@@ -26,27 +26,20 @@ public class FrontController extends HttpServlet {
 		System.out.println("Servlet Loaded");
 		resp.setContentType("text/html; charset=UTF-8");
 
-		// 클라이언트가 요청한 주소 알아내기
-		String requestPath = req.getServletPath();
 		try {
-
-			String controllerPath = null;
-			if ("/join".equals(requestPath)) {
-				controllerPath = "/join";
-				// 회원가입 정보를 저장
-				req.setAttribute("member", new Member()
-						.setMem_email(req.getParameter("email"))
-						.setMem_password(req.getParameter("password"))
-						);
-			}
+			ControllerFactory factory = ControllerFactory.getInstance();
+			Controller controller = factory.createController(req.getServletPath());
 
 			// 인클루드하여 처리 위임
-			RequestDispatcher rd = req.getRequestDispatcher(controllerPath);
+			RequestDispatcher rd = req.getRequestDispatcher("");
 			rd.include(req, resp);
 
-			String viewURL = (String) req.getAttribute("viewURL");
-			rd = req.getRequestDispatcher(viewURL);
+			// 담당 컨트롤러가 반환하는 뷰의 URL을 이용하여 뷰에게 실행을 위임
+			// 조건에 따라 위임 방식으로 포워딩 혹은 인클루딩을 사용
+			String viewUrl = (String) req.getAttribute("viewUrl");
+			rd = req.getRequestDispatcher(viewUrl);
 			rd.include(req, resp);
+			//rd.forward(req, resp);
 		} catch (Exception e) {
 			e.printStackTrace();
 			// 에러를 세션에 저장
