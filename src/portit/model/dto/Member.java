fe1fec49c26652bc28e81931cd35a21fad243faf
Suyeon <syeon___@hanmail.net>
2017-07-07 19:30:03 +0900

    dto 수정

--- src/portit/model/dto/Member.java
+++ src/portit/model/dto/Member.java
@@ -24,7 +24,7 @@ public class Member {
 	public int getProf_id(){
 		return prof_id;
 	}
-	public void setProf_if(int prof_id){
+	public void setProf_id(int prof_id){
 		this.prof_id = prof_id;
 	}
 	
