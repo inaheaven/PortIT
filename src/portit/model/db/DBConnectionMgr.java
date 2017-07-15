c6582c4b4490e66637c9c77cb86bdc84ce45d7ad
Cater Jo <kin7729@naver.com>
2017-07-15 16:22:21 +0900

    0715_데이터추가

--- src/portit/model/db/DBConnectionMgr.java
+++ src/portit/model/db/DBConnectionMgr.java
@@ -45,7 +45,7 @@ public class DBConnectionMgr {
     
     private boolean _traceOn = false;
     private boolean initialized = false;
-    private int _openConnections = 50;
+    private int _openConnections = 5000;
     private static DBConnectionMgr instance = null;
 
     public DBConnectionMgr() {
